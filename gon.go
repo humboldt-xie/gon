114887700053020f9a5f9df1d8cd82962531d706
humboldt <humboldt_xie@163.com>
2018-03-27 22:32:52 +0800

    init

---
+++ gon.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"context"
+	"fmt"
+	"log"
+	"reflect"
+	"runtime"
+	"strings"
+)
+
+func GetFunctionName(m reflect.Value) string {
+	return runtime.FuncForPC(m.Pointer()).Name()
+}
+
+func JsonMap(c interface{}) {
+	v := reflect.ValueOf(c)
+	for i := 0; i < v.NumMethod(); i++ {
+		method := v.Type().Method(i)
+		log.Printf("method:%s\n", method.Name)
+		numin := v.Method(i).Type().NumIn()
+		in := v.Method(i).Type().In(numin - 1)
+		inPut := reflect.New(in.Elem())
+		log.Printf("new input:%#v\n", inPut)
+	}
+}
+
+// input   (context.Context,request) or (request)
+// return  Object or (Object,error)
+
+func GinRPC(r gin.IRouter, v reflect.Value, method reflect.Method) {
+	log.Printf("gin rpc method:%s", method.Name)
+	handler := func(c *gin.Context) {
+		num := v.Type().NumIn()
+		inValue := make([]reflect.Value, 0)
+		if num > 0 {
+			in := v.Type().In(num - 1)
+			inPut := reflect.New(in.Elem())
+			err := c.BindJSON(inPut.Interface())
+			if err != nil {
+				c.String(401, fmt.Sprintf("%s", err))
+				return
+			}
+			if num > 1 {
+				ctx := reflect.ValueOf(context.Background())
+				inValue = append(inValue, ctx)
+				inValue = append(inValue, inPut)
+			} else {
+				inValue = append(inValue, inPut)
+			}
+		}
+		//log.Printf("bind input:%#v %s\n", inPut, err)
+		response := v.Call(inValue)
+		//log.Printf("output :%#v \n", response)
+		if len(response) > 1 {
+			if response[1].Interface() != nil {
+				c.String(500, fmt.Sprintf("%s", response[1].Interface()))
+				return
+			}
+		}
+		c.JSON(200, response[0].Interface())
+	}
+	lowerName := strings.ToLower(method.Name)
+	if lowerName != method.Name {
+		r.POST(lowerName, handler)
+	}
+	r.POST(method.Name, handler)
+}
+
+func RegisterInterface(name string, r gin.IRouter, c interface{}) {
+	group := r.Group("/" + name)
+	v := reflect.ValueOf(c)
+	for i := 0; i < v.NumMethod(); i++ {
+		method := v.Type().Method(i)
+		GinRPC(group, v.Method(i), method)
+	}
+
+}
+
+func NewHttp(name string, i interface{}, listen string) {
+	eg := gin.New()
+	JsonMap(i)
+	RegisterInterface(name, eg, i)
+	eg.Run(listen)
+}
---
+++ gon_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type myHttp struct {
+}
+
+type param struct {
+	Param string
+}
+type result struct {
+	Result string
+}
+
+func (m *myHttp) Request2(ctx context.Context, h *param) result {
+	return result{Result: "201"}
+}
+
+func (m *myHttp) Request(h *param) result {
+	return result{Result: "200"}
+}
+
+func post(engine *gin.Engine, path string, postbody string) (*http.Response, string) {
+	//构建返回值
+	w := httptest.NewRecorder()
+	//构建请求
+	r, _ := http.NewRequest("POST", path, strings.NewReader(postbody))
+	//调用请求接口
+	engine.ServeHTTP(w, r)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	return resp, string(body)
+}
+
+func newEngine(in interface{}) *gin.Engine {
+	gin.SetMode(gin.TestMode)
+	eg := gin.New()
+	RegisterInterface("path", eg, in)
+	return eg
+}
+
+func TestMap(t *testing.T) {
+	eg := newEngine(&myHttp{})
+
+	resp, body := post(eg, "/path/request2", `{"Param":""}`)
+
+	t.Log(resp.StatusCode)
+	//获得结果，并检查
+	if string(body) != `{"Result":"201"}` {
+		t.Fatal(body)
+	}
+
+}
